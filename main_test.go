49f80ebc9410251a9a669a12497d54df35977684
Minh Phạm Vũ <minh.pv@teko.vn>
2022-10-11 23:51:51 +0700

    Add test cases

--- main_test.go
+++ main_test.go
@@ -5,52 +5,57 @@ import (
 	"testing"
 )
 
-func TestComputeDayAgo_Min_Date(t *testing.T) {
-	result := computeDayAgo(newTime("2022-08-10T00:00:00+07:00"), 7)
-	assert.Equal(t, newTime("2022-08-03T00:00:00+07:00").Unix(), result)
+func TestComputeDayAgo_Nom_Date_Nom_Limit(t *testing.T) {
+	result := computeDayAgo(newTime("2022-10-11T12:00:00+07:00"), 7)
+	assert.Equal(t, newTime("2022-10-05T00:00:00+07:00").Unix(), result)
 }
 
-func TestComputeDayAgo_Max_Date(t *testing.T) {
-	result := computeDayAgo(newTime("2022-08-05T23:59:59+07:00"), 7)
-	assert.Equal(t, newTime("2022-07-30T00:00:00+07:00").Unix(), result)
+func TestComputeDayAgo_Nom_Date_Min_Limit(t *testing.T) {
+	result := computeDayAgo(newTime("2022-10-11T12:00:00+07:00"), 1)
+	assert.Equal(t, newTime("2022-10-11T00:00:00+07:00").Unix(), result)
 }
 
-func TestComputeDayAgo_MinPlus_Date(t *testing.T) {
-	result := computeDayAgo(newTime("2022-08-10T00:00:01+07:00"), 7)
-	assert.Equal(t, newTime("2022-08-04T00:00:00+07:00").Unix(), result)
+func TestComputeDayAgo_Nom_Date_MinPlus_Limit(t *testing.T) {
+	result := computeDayAgo(newTime("2022-10-11T12:00:00+07:00"), 2)
+	assert.Equal(t, newTime("2022-10-10T00:00:00+07:00").Unix(), result)
 }
 
-func TestComputeDayAgo_MaxMinus_Date(t *testing.T) {
-	result := computeDayAgo(newTime("2022-08-05T23:59:58+07:00"), 7)
-	assert.Equal(t, newTime("2022-07-30T00:00:00+07:00").Unix(), result)
+func TestComputeDayAgo_Nom_Date_MaxMinus_Limit(t *testing.T) {
+	result := computeDayAgo(newTime("2022-10-11T12:00:00+07:00"), 29)
+	assert.Equal(t, newTime("2022-09-13T00:00:00+07:00").Unix(), result)
 }
 
-func TestComputeDayAgo_Nom_Date(t *testing.T) {
-	result := computeDayAgo(newTime("2022-08-05T10:00:00+07:00"), 7)
-	assert.Equal(t, newTime("2022-07-30T00:00:00+07:00").Unix(), result)
+func TestComputeDayAgo_Nom_Date_Max_Limit(t *testing.T) {
+	result := computeDayAgo(newTime("2022-10-11T12:00:00+07:00"), 30)
+	assert.Equal(t, newTime("2022-09-12T00:00:00+07:00").Unix(), result)
 }
 
-func TestComputeDayAgo_Min_Limit(t *testing.T) {
-	result := computeDayAgo(newTime("2022-08-05T12:00:00+07:00"), 1)
-	assert.Equal(t, newTime("2022-08-05T00:00:00+07:00").Unix(), result)
+func TestComputeDayAgo_Min_Date_Nom_Limit(t *testing.T) {
+	result := computeDayAgo(newTime("2022-10-15T00:00:00+07:00"), 15)
+	assert.Equal(t, newTime("2022-09-30T00:00:00+07:00").Unix(), result)
 }
 
-func TestComputeDayAgo_Max_Limit(t *testing.T) {
-	result := computeDayAgo(newTime("2022-08-05T10:00:00+07:00"), 30)
-	assert.Equal(t, newTime("2022-07-07T00:00:00+07:00").Unix(), result)
+func TestComputeDayAgo_MinPlus_Date_Nom_Limit(t *testing.T) {
+	result := computeDayAgo(newTime("2022-10-15T00:00:01+07:00"), 15)
+	assert.Equal(t, newTime("2022-10-01T00:00:00+07:00").Unix(), result)
 }
 
-func TestComputeDayAgo_MinPlus_Limit(t *testing.T) {
-	result := computeDayAgo(newTime("2022-08-05T10:00:00+07:00"), 2)
-	assert.Equal(t, newTime("2022-08-04T00:00:00+07:00").Unix(), result)
+func TestComputeDayAgo_Max_Date_Nom_Limit(t *testing.T) {
+	result := computeDayAgo(newTime("2022-10-15T23:59:59+07:00"), 15)
+	assert.Equal(t, newTime("2022-10-01T00:00:00+07:00").Unix(), result)
 }
 
-func TestComputeDayAgo_MaxMinus_Limit(t *testing.T) {
-	result := computeDayAgo(newTime("2022-08-05T10:00:00+07:00"), 29)
-	assert.Equal(t, newTime("2022-07-08T00:00:00+07:00").Unix(), result)
+func TestComputeDayAgo_MaxMinus_Date_Nom_Limit(t *testing.T) {
+	result := computeDayAgo(newTime("2022-10-15T23:59:58+07:00"), 15)
+	assert.Equal(t, newTime("2022-10-01T00:00:00+07:00").Unix(), result)
 }
 
-func TestComputeDayAgo_Nom_Limit(t *testing.T) {
-	result := computeDayAgo(newTime("2022-08-05T10:00:00+07:00"), 15)
-	assert.Equal(t, newTime("2022-07-22T00:00:00+07:00").Unix(), result)
+func TestComputeDayAgo_Limit_Overbound(t *testing.T) {
+	result := computeDayAgo(newTime("2022-10-15T23:59:58+07:00"), 31)
+	assert.Equal(t, int64(0), result)
+}
+
+func TestComputeDayAgo_Limit_Underbound(t *testing.T) {
+	result := computeDayAgo(newTime("2022-10-15T23:59:58+07:00"), 0)
+	assert.Equal(t, int64(0), result)
 }
